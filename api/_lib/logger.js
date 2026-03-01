c1b93eca13c5fd2a9f88cf3f78a929ebc15fd1e1
franselsimmons <lrsdaktechniek@hotmail.com>
2026-03-01 19:45:54 +0100

    Create logger.js

---
+++ api/_lib/logger.js
@@ -0,0 +1,24 @@
+const winston = require("winston");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports: []
+});
+
+logger.add(
+  new winston.transports.Console({
+    format: isProduction
+      ? winston.format.combine(winston.format.timestamp(), winston.format.json())
+      : winston.format.combine(winston.format.colorize(), winston.format.simple())
+  })
+);
+
+module.exports = logger;
