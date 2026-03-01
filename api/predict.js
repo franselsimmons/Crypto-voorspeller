bec385400db5d4b95a2f6858c29bf5e38374c8f0
franselsimmons <lrsdaktechniek@hotmail.com>
2026-03-01 20:40:30 +0100

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -1,14 +1,14 @@
 /* EOF: /api/predict.js */
-const { predict } = require("./_lib/forestEngine"); // _lib, niet lib
+const { predict } = require("./_lib/forestEngine");
 
-module.exports = async function handler(req, res) {
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Use POST" });
+  }
+
   try {
-    if (req.method !== "POST") {
-      return res.status(405).json({ error: "Use POST" });
-    }
-
     const prediction = await predict();
-    return res.status(200).json({ prediction });
+    return res.status(200).json({ ok: true, prediction });
   } catch (e) {
     return res.status(500).json({
       error: "Prediction failed",
