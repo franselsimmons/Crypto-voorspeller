ce87436258757dc7245d3d9f18bf468692de094e
franselsimmons <lrsdaktechniek@hotmail.com>
2026-05-20 16:29:06 +0200

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <main className="shell">
+      <div className="panel">
+        <div className="panel-title">Dashboard laden...</div>
+        <div className="panel-subtitle">Database wordt uitgelezen.</div>
+      </div>
+    </main>
+  );
+}
